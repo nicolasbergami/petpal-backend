80425f4d35006537bfaa17e79603db84e05da0d9
Nicolas Bergami <nicolasbergami2013@gmail.com>
2025-07-19 18:34:42 -0300

    se cambio yml

--- src/config/db.js
+++ src/config/db.js
@@ -1,32 +1,28 @@
+// src/config/db.js
 const mysql = require('mysql2');
-require('iconv-lite').encodingExists('cesu8'); // ⚠️ Necesario para evitar el error en Railway
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
-const connection = mysql.createConnection({
-    /*//PARA SUBIR RAILWAY
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
+let connection;
+if (process.env.DATABASE_URL) {
+  // Conexion directa usando URL (incluye usuario, password, host, puerto y database)
+  connection = mysql.createConnection(process.env.DATABASE_URL);
+} else {
+  // modo local o Azure DevOps con variables sueltas
+  connection = mysql.createConnection({
+    host:     process.env.DB_HOST,
+    user:     process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME, // ✅ Aquí cambié a DB_NAME en vez de DB_DATABASE
-    port: process.env.DB_PORT       // ✅ Asegurarnos de que tome el puerto correctamente
-    */
-   //PARA LOCAL
-   host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE, // <- esto estaba mal como DB_NAME
-    port: process.env.DB_PORT
+    database: process.env.DB_DATABASE,
+    port:     process.env.DB_PORT
+  });
+}
 
-});
-
-connection.connect((err) => {
-    if (err) {
-        console.error('❌ Error conectando a la base de datos:', err.message);
-        return;
-    }
-    console.log('🚀 Conectado a la base de datos MySQL');
+connection.connect(err => {
+  if (err) {
+    console.error('❌ Error conectando a la base de datos:', err.message);
+    return;
+  }
+  console.log('🚀 Conectado a la base de datos MySQL');
 });
 
 module.exports = connection;
-
