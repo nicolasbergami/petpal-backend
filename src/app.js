e3f0c9fd731273eac8018a8e72a68d529aed8832
santiaguero7 <aguero17.2001@gmail.com>
2026-02-09 17:39:23 -0300

    Actualizacion backend para QA

--- src/app.js
+++ src/app.js
@@ -60,7 +60,7 @@ console.log("   ➡️  /api/auth registrado correctamente");
 
 // Ruta principal
 app.get('/', (req, res) => {
-    res.send('Petpal API funcionando correctamente 🚀 con CORS dinámico para Vercel prueba Santi solo ');
+    res.send('Petpal API funcionando correctamente 🚀 con CORS dinámico para Vercel prueba Santi por segunda vez ');
 });
 
 module.exports = app;
