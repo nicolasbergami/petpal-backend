20f4f091a461ca87debc1ba0734bf5a309e006d5
Nicolas Bergami <nicolasbergami@Mac.fibertel.com.ar>
2026-02-10 01:59:23 -0300

    test pasados positivamente

--- src/app.js
+++ src/app.js
@@ -60,7 +60,7 @@ console.log("   ➡️  /api/auth registrado correctamente");
 
 // Ruta principal
 app.get('/', (req, res) => {
-    res.send('Petpal API funcionando correctamente 🚀 con CORS dinámico para Vercel PRUEBA CON FELI ');
+    res.send('Petpal API funcionando correctamente 🚀 con CORS dinámico para Vercel TEST PASADOS BIEN ');
 });
 
 module.exports = app;
